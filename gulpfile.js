c92cfb9b5a40adbdc1f7ebf0d817a96f8d0938a9
Ben Rogerson <info@benrogerson.com.au>
2018-09-05 07:43:49 +0930

    remove returns

--- gulpfile.js
+++ gulpfile.js
@@ -116,147 +116,146 @@ const compressScripts = () => (
 /**
  * Delete files from various directories
  */
-gulp.task('clean', () => {
-    const filesFolders = [
+gulp.task('clean', () => (
+    gulp.src([
         pkg.config.versions, // Remove versions file
         pkg.config.public.assets, // Remove whole built assets folder
-    ];
-    return gulp.src(filesFolders, {read: false}).pipe($.clean());
-});
+    ], {read: false}).pipe($.clean())
+));
 
 /**
  * Handle project styles
  */
-gulp.task('styles', () => {
-    return gulp.src(pkg.config.stylesMain.source)
-        .pipe($.plumber({errorHandler: handleError}))
-        .pipe($.sassVariables({
-            $isDev: isDev
-        }))
-        .pipe($.sass({
-            includePaths: ['node_modules']
-        }))
-        .pipe($.cached('styles'))
-        .pipe($.autoprefixer({
-            browsers: [
-                '> 0.5% in AU',
-                'last 3 years',
-                'iOS >= 7',
-                'ie >= 10'
-            ]
-        }))
-        .pipe(
-            $.if(config.compress,
-            $.cssnano({
-                safe: true,
-                autoprefixer: false,
-                discardComments: {
-                    removeAll: true
-                },
-                discardDuplicates: true,
-                discardEmpty: true,
-                minifyFontValues: false,
-                minifySelectors: true
-            }),
-        ))
-        .pipe($.size({gzip: true, showFiles: true}))
-        .pipe($.rename(pkg.config.stylesMain.destination))
-        .pipe($.if(isProd, $.rev()))
-        .pipe(gulp.dest(pkg.config.public.base))
-        .pipe($.if(isProd, writeVersionFile()))
-        .pipe($.browserSync.stream({match: '**/*.css'}));
-});
+gulp.task('styles', () => (
+    gulp.src(pkg.config.stylesMain.source)
+    .pipe($.plumber({errorHandler: handleError}))
+    .pipe($.sassVariables({
+        $isDev: isDev
+    }))
+    .pipe($.sass({
+        includePaths: ['node_modules']
+    }))
+    .pipe($.cached('styles'))
+    .pipe($.autoprefixer({
+        browsers: [
+            '> 0.5% in AU',
+            'last 3 years',
+            'iOS >= 7',
+            'ie >= 10'
+        ]
+    }))
+    .pipe(
+        $.if(config.compress,
+        $.cssnano({
+            safe: true,
+            autoprefixer: false,
+            discardComments: {
+                removeAll: true
+            },
+            discardDuplicates: true,
+            discardEmpty: true,
+            minifyFontValues: false,
+            minifySelectors: true
+        }),
+    ))
+    .pipe($.size({gzip: true, showFiles: true}))
+    .pipe($.rename(pkg.config.stylesMain.destination))
+    .pipe($.if(isProd, $.rev()))
+    .pipe(gulp.dest(pkg.config.public.base))
+    .pipe($.if(isProd, writeVersionFile()))
+    .pipe($.browserSync.stream({match: '**/*.css'}))
+));
 
 /**
  * Main script task
  */
-gulp.task('scripts:main', () => {
-    return gulp.src(pkg.config.scriptsMain.source)
-        .pipe(browserify())
-        .pipe($.if(config.compress, compressScripts()))
-        .pipe($.size({gzip: true, showFiles: true}))
-        .pipe($.rename(pkg.config.scriptsMain.destination))
-        .pipe($.if(isProd, $.rev()))
-        .pipe(gulp.dest(pkg.config.public.base))
-        .pipe($.if(isProd, writeVersionFile()))
-        .pipe($.browserSync.stream({match: '**/*.js'}));
-});
+gulp.task('scripts:main', () => (
+    gulp.src(pkg.config.scriptsMain.source)
+    .pipe(browserify())
+    .pipe($.if(config.compress, compressScripts()))
+    .pipe($.size({gzip: true, showFiles: true}))
+    .pipe($.rename(pkg.config.scriptsMain.destination))
+    .pipe($.if(isProd, $.rev()))
+    .pipe(gulp.dest(pkg.config.public.base))
+    .pipe($.if(isProd, writeVersionFile()))
+    .pipe($.browserSync.stream({match: '**/*.js'}))
+));
 
 /**
  * Scripts > Create additional javascript files.
  * These scripts are for loading on specific pages.
  */
-gulp.task('scripts:singles', () => {
-    return gulp.src(pkg.config.scriptsSingles.source)
-        .pipe($.plumber({errorHandler: handleError}))
-        .pipe($.if(['*.js'],
-            $.newer({dest: pkg.config.scriptsSingles.destination})
-        ))
-        .pipe(browserify())
-        .pipe($.if(config.compress, compressScripts()))
-        .pipe($.rename({dirname: pkg.config.scriptsSingles.destination}))
-        .pipe($.size({gzip: true, showFiles: true}))
-        .pipe($.if(isProd, $.rev()))
-        .pipe(gulp.dest(pkg.config.public.base))
-        .pipe($.if(isProd, writeVersionFile()));
-});
+gulp.task('scripts:singles', () => (
+    gulp.src(pkg.config.scriptsSingles.source)
+    .pipe($.plumber({errorHandler: handleError}))
+    .pipe($.if(['*.js'],
+        $.newer({dest: pkg.config.scriptsSingles.destination})
+    ))
+    .pipe(browserify())
+    .pipe($.if(config.compress, compressScripts()))
+    .pipe($.rename({dirname: pkg.config.scriptsSingles.destination}))
+    .pipe($.size({gzip: true, showFiles: true}))
+    .pipe($.if(isProd, $.rev()))
+    .pipe(gulp.dest(pkg.config.public.base))
+    .pipe($.if(isProd, writeVersionFile()))
+));
 
 /**
  * JS > Combines scripts into a single 'assets/js/plugins.js' package
  * Runs once at the beginning of the dev/prod process
  */
-gulp.task('scripts:vendor', () => {
-    return gulp.src(pkg.config.scriptsVendor.source)
+gulp.task('scripts:vendor', () => (
+    gulp.src(pkg.config.scriptsVendor.source)
     .pipe($.plumber({errorHandler: handleError}))
-        .pipe(browserify())
-        .pipe($.if(config.compress, compressScripts()))
-        .pipe($.concat(pkg.config.scriptsVendor.destination))
-        .pipe($.size({gzip: true, showFiles: true}))
-        .pipe($.if(isProd, $.rev()))
-        .pipe(gulp.dest(pkg.config.public.base))
-        .pipe($.if(isProd, writeVersionFile()))
-        .pipe($.browserSync.stream({match: '**/*.js'}));
-});
+    .pipe(browserify())
+    .pipe($.if(config.compress, compressScripts()))
+    .pipe($.concat(pkg.config.scriptsVendor.destination))
+    .pipe($.size({gzip: true, showFiles: true}))
+    .pipe($.if(isProd, $.rev()))
+    .pipe(gulp.dest(pkg.config.public.base))
+    .pipe($.if(isProd, writeVersionFile()))
+    .pipe($.browserSync.stream({match: '**/*.js'}))
+));
 
 /**
  * Images > Compress images/vectors
  */
-gulp.task('images', () => {
-    return gulp.src(pkg.config.images.source)
-        .pipe($.newer({dest: `${pkg.config.public.base}/${pkg.config.images.destination}`}))
-        .pipe($.imagemin([
-            $.imagemin.gifsicle({interlaced: true}),
-            $.imagemin.jpegtran({progressive: true}),
-            $.imagemin.optipng({optimizationLevel: 7}),
-            $.imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
-        .pipe($.cached('images'))
-        .pipe($.rename({dirname: pkg.config.images.destination}))
-        .pipe($.if(isProd, $.rev()))
-        .pipe(gulp.dest(pkg.config.public.base))
-        .pipe($.if(isProd, writeVersionFile()));
-});
+gulp.task('images', () => (
+    gulp.src(pkg.config.images.source)
+    .pipe($.newer({dest: `${pkg.config.public.base}/${pkg.config.images.destination}`}))
+    .pipe($.imagemin([
+        $.imagemin.gifsicle({interlaced: true}),
+        $.imagemin.jpegtran({progressive: true}),
+        $.imagemin.optipng({optimizationLevel: 7}),
+        $.imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+    .pipe($.cached('images'))
+    .pipe($.rename({dirname: pkg.config.images.destination}))
+    .pipe($.if(isProd, $.rev()))
+    .pipe(gulp.dest(pkg.config.public.base))
+    .pipe($.if(isProd, writeVersionFile()))
+));
 
 /**
  * SVG Icon > Combine a series of svgs into a single sprite in 'assets/icons.svg'
  */
-gulp.task('icons', () => {
-    return gulp.src(pkg.config.icons.source)
-        .pipe($.plumber({errorHandler: handleError}))
-        .pipe($.svgmin())
-        .pipe($.rename({prefix: 'icon-'}))
-        .pipe($.svgstore())
-        .pipe($.rename(pkg.config.icons.destination))
-        .pipe($.if(isProd, $.rev()))
-        .pipe(gulp.dest(pkg.config.public.base))
-        .pipe($.if(isProd, writeVersionFile()))
-        .pipe($.browserSync.stream({match: '**/*.svg'}));
-});
+gulp.task('icons', () => (
+    gulp.src(pkg.config.icons.source)
+    .pipe($.plumber({errorHandler: handleError}))
+    .pipe($.svgmin())
+    .pipe($.rename({prefix: 'icon-'}))
+    .pipe($.svgstore())
+    .pipe($.rename(pkg.config.icons.destination))
+    .pipe($.if(isProd, $.rev()))
+    .pipe(gulp.dest(pkg.config.public.base))
+    .pipe($.if(isProd, writeVersionFile()))
+    .pipe($.browserSync.stream({match: '**/*.svg'}))
+));
 
 
 // We run in sequence so build files can be deleted first and to avoid
